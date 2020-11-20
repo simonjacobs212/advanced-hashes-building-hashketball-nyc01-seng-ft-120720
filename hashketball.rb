85d928890bfcccf539993376d1a28f0f67f0a482
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-20 17:11:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -75,6 +75,50 @@ def game_hash
           :steals => 0,
           :blocks => 0,
           :slam_dunks => 0
+        },
+        {
+          :player_name => nil,
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
+        {
+          :player_name => nil,
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
+        {
+          :player_name => nil,
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
+        {
+          :player_name => nil,
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
         }
       ]
     }
