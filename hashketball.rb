fcefd8ec3894b9f315ca4681d6fdee707fed10a3
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-20 17:16:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -66,26 +66,26 @@ def game_hash
       :colors => [],
       :players => [
         {
-          :player_name => nil,
-          :number => 0,
-          :shoe => 0,
-          :points => 0,
-          :rebounds => 0,
-          :assists => 0,
-          :steals => 0,
-          :blocks => 0,
-          :slam_dunks => 0
+          :player_name => "Jeff Adrien",
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
         },
         {
-          :player_name => nil,
+          :player_name => "Bismack Biyombo",
           :number => 0,
-          :shoe => 0,
-          :points => 0,
-          :rebounds => 0,
-          :assists => 0,
-          :steals => 0,
-          :blocks => 0,
-          :slam_dunks => 0
+          :shoe => 16,
+          :points => 12,
+          :rebounds => 4,
+          :assists => 7,
+          :steals => 22,
+          :blocks => 15,
+          :slam_dunks => 10
         },
         {
           :player_name => nil,
