0229320f7451f0c62534a1d63009b51c9dd0dca8
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-20 17:13:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -38,7 +38,7 @@ def game_hash
           :slam_dunks => 15
         },
         {
-          :player_name => nil,
+          :player_name => "Mason Plumlee",
           :number => 0,
           :shoe => 0,
           :points => 0,
