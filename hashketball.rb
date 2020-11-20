60859fa667b775ada2b971520022e3605e2ef8a8
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-20 17:10:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,52 @@ def game_hash
   hash = {
     :home => {
       :team_name => "Brooklyn Nets",
-      :colors => [],
+      :colors => ["Black", "White"],
       :players => [
+        {
+          :player_name => nil,
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
+        {
+          :player_name => nil,
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
+        {
+          :player_name => nil,
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
+        {
+          :player_name => nil,
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        },
         {
           :player_name => nil,
           :number => 0,
