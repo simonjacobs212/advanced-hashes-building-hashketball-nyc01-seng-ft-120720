5b021f7a251422ae1d3aad96587b6199d104cd4b
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-20 17:19:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,7 +63,7 @@ def game_hash
     },
     :away => {
       :team_name => "Charlotte Hornets",
-      :colors => [],
+      :colors => ["Turquoise", "Purple"],
       :players => [
         {
           :player_name => "Jeff Adrien",
