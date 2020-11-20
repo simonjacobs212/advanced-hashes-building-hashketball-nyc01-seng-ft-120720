a3c2d19adf8dc08534005a17ea284a299bf0a568
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-20 17:12:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,24 +7,24 @@ def game_hash
         {
           :player_name => "Alan Anderson",
           :number => 0,
-          :shoe => 0,
-          :points => 0,
-          :rebounds => 0,
-          :assists => 0,
-          :steals => 0,
-          :blocks => 0,
-          :slam_dunks => 0
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
         },
         {
-          :player_name => nil,
-          :number => 0,
-          :shoe => 0,
-          :points => 0,
-          :rebounds => 0,
-          :assists => 0,
-          :steals => 0,
-          :blocks => 0,
-          :slam_dunks => 0
+          :player_name => "Reggie Evans",
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7
         },
         {
           :player_name => nil,
